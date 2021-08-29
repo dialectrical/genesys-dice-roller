bb16d47d26e4ec4b72cad1b13510d6b48efec506
dialectrical <>
2021-08-29 14:12:57 -0700

    implements Material UI Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { React, useState } from "react";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import { DICE_MATRIX, DICE_NAMES } from "./Components/Constants.js";
 import OutComeTally from "./Components/Outcome.js";
 import AmountSetter from "./Components/AmountSetter.js";
@@ -11,8 +13,8 @@ export const App = () => {
 
   const handleChange = event => {
     AmountSetter(
-      event.target.id,
-      event.target.value,
+      event.currentTarget.id,
+      event.currentTarget.value,
       rollAmount,
       setRollAmount
     );
@@ -23,7 +25,7 @@ export const App = () => {
   };
 
   return (
-    <Grid container direction="row" spacing={12} alignItems="center">
+    <Grid container direction="row" spacing={6} alignItems="center">
       <Grid item direciton="column" xs={6} sm={3}>
         {DICE_MATRIX.map(x => {
           return (
@@ -31,32 +33,44 @@ export const App = () => {
               <h2 className={DICE_NAMES[DICE_MATRIX.indexOf(x)]}>
                 {DICE_NAMES[DICE_MATRIX.indexOf(x)]}
               </h2>
-              <button
-                value={-1}
-                id={DICE_MATRIX.indexOf(x)}
-                onClick={handleChange}
-              >
-                -
-              </button>
-              {rollAmount[DICE_MATRIX.indexOf(x)]}
-              <button
-                value={1}
-                id={DICE_MATRIX.indexOf(x)}
-                onClick={handleChange}
-              >
-                +
-              </button>
+              <ButtonGroup size="large" variant="contained" color="primary">
+                <Button
+                  value={-1}
+                  id={DICE_MATRIX.indexOf(x)}
+                  onClick={handleChange}
+                >
+                  -
+                </Button>
+                <Button disabled variant="outlined">
+                  {rollAmount[DICE_MATRIX.indexOf(x)]}
+                </Button>
+                <Button
+                  value={1}
+                  id={DICE_MATRIX.indexOf(x)}
+                  onClick={handleChange}
+                >
+                  +
+                </Button>
+              </ButtonGroup>
             </Grid>
           );
         })}
-        <button onClick={handleRoll}>Roll</button>
+        <Grid item>
+          <Button
+            size="large"
+            color="secondary"
+            variant="outlined"
+            onClick={handleRoll}
+          >
+            Roll
+          </Button>
+        </Grid>
       </Grid>
       <Grid
         container
         item
         direction="row"
-        justifyContent="flex-end"
-        item
+        justifyContent="flex-start"
         xs={6}
         sm={9}
       >
