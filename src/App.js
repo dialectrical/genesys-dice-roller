ea5033a3e308057f6137720ed5508b0944399ef0
dialectrical <>
2021-08-30 15:49:55 -0700

    adds more styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import { DICE_MATRIX, DICE_NAMES } from "./Components/Constants.js";
 import OutcomeTally from "./Components/Outcome.js";
 import AmountSetter from "./Components/AmountSetter.js";
 import ResultSetter from "./Components/ResultSetter";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    margin: "100px 640px 100px 640px",
+    padding: "0px 20px 0px 20px"
+  }
+});
 
 export const App = () => {
   const [rollAmount, setRollAmount] = useState([0, 0, 0, 0, 0, 0]);
   const [result, setResult] = useState();
+  const classes = useStyles();
 
   const handleChange = event => {
     AmountSetter(
@@ -28,15 +38,15 @@ export const App = () => {
 
   return (
     <CssBaseline>
-      <Paper>
+      <Paper elevation={6} classes={{ root: classes.root }}>
         <Grid
           container
           direction="row"
-          spacing={6}
+          spacing={3}
           alignItems="center"
-          justifyContent="center"
+          justifyContent="flex-start"
         >
-          <Grid item md="auto" direciton="column" xs={6} sm={3}>
+          <Grid item md="auto" direciton="column">
             {DICE_MATRIX.map(x => {
               return (
                 <Grid item>
@@ -52,7 +62,9 @@ export const App = () => {
                       -
                     </Button>
                     <Button disabled variant="outlined">
-                      {rollAmount[DICE_MATRIX.indexOf(x)]}
+                      <Typography color="textPrimary">
+                        {rollAmount[DICE_MATRIX.indexOf(x)]}
+                      </Typography>
                     </Button>
                     <Button
                       value={1}
@@ -78,12 +90,13 @@ export const App = () => {
           </Grid>
           <Grid
             container
-            item
+            spacing={4}
+            alignItems="center"
+            justifyContent="space-evenly"
             direction="row"
-            justifyContent="flex-start"
-            lg={4}
-            xs={6}
-            sm={6}
+            lg={9}
+            xs={9}
+            sm={9}
           >
             {result ? OutcomeTally(result) : ""}
           </Grid>
--- src/Components/Outcome.js
+++ src/Components/Outcome.js
@@ -1,5 +1,9 @@
 import { RESULT_NAMES } from "./Constants.js";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 export const OutcomeTally = arr => {
   let i = -1;
@@ -14,26 +18,22 @@ export const OutcomeTally = arr => {
     if (x >= 0) {
       v++;
       return (
-        <Grid container direction="column" item xs={4} sm={3}>
-          <Grid item>
-            <h2>{RESULT_NAMES[v]}</h2>
-          </Grid>
-          <Grid item>
-            <h2>{x}</h2>
-          </Grid>
-        </Grid>
+        <Card raised>
+          <CardContent>
+            <Typography color="textPrimary">{RESULT_NAMES[v]}</Typography>
+            <Typography align="center">{x}</Typography>
+          </CardContent>
+        </Card>
       );
     } else if (x < 0) {
       v++;
       return (
-        <Grid container direction="column" item xs={4} sm={3}>
-          <Grid item>
-            <h2>{RESULT_NAMES[v + 3]}</h2>
-          </Grid>{" "}
-          <Grid item>
-            <h2>{Math.abs(x)}</h2>
-          </Grid>
-        </Grid>
+        <Card raised>
+          <CardContent>
+            <Typography color="textPrimary">{RESULT_NAMES[v + 3]}</Typography>
+            <Typography align="center">{Math.abs(x)}</Typography>
+          </CardContent>
+        </Card>
       );
     } else {
       return (
